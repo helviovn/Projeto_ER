ef3c4af8a932051b1896f2b7348bb24aec08ee33
artur <artursfreitas@hotmail.com>
2022-12-05 23:46:33 +0000

    Margens feitas

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -7,10 +7,10 @@
     <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="header">
+        <div class="header" style="background-image: url('img/Info.png');!important; display: block; background-size: cover;">
         <div class="side-nav">
             <a href="#" class="logo">
-                <img src="img/LOGO33.png" class="logo-img" style="width: 220px" alt="">
+                <img src="img/LOGO33.png" class="logo-img" alt="">
             </a>
             <ul class="nav-links">
                 <li><a href="#"><i class="fa-solid fa-car-burst"></i><p>Acidente</p></a></li>
@@ -21,6 +21,14 @@
                 <li><a href="#"><p>Acidente</p></a></li>
                 <div class="active"></div>
             </ul>
+        </div>
+            <i class="fa-solid fa-map-location-dot" style="position: absolute"></i>
+
+        <div class="ecra">
+            <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBBBaaaaaaaaaaaaaaaaaa</h1>
+            <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBBBaaaaaaaaaaaaaaa</h1>
+            <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBBBaaaaaaaaaaaaaa</h1>
+
         </div>
     </div>
 
