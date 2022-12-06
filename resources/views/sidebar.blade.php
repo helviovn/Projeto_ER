f6d02ead2069a1b3e25f588d4760becd34ea9525
pedroandrade4059 <pedrogsandrade4059@gmail.com>
2022-12-06 00:32:37 +0000

    Container

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -7,29 +7,32 @@
     <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
 </head>
 <body>
-        <div class="header" style="background-image: url('img/Info.png');!important; display: block; background-size: cover;">
-        <div class="side-nav">
-            <a href="#" class="logo">
-                <img src="img/LOGO33.png" class="logo-img" alt="">
-            </a>
-            <ul class="nav-links">
-                <li><a href="#"><i class="fa-solid fa-car-burst"></i><p>Acidente</p></a></li>
-                <li><a href="#"><p>Acidente</p></a></li>
-                <li><a href="#"><p>Acidente</p></a></li>
-                <li><a href="#"><p>Acidente</p></a></li>
-                <li><a href="#"><p>Acidente</p></a></li>
-                <li><a href="#"><p>Acidente</p></a></li>
-                <div class="active"></div>
-            </ul>
-        </div>
-            <i class="fa-solid fa-map-location-dot" style="position: absolute"></i>
-
-        <div class="ecra">
-            <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBBBaaaaaaaaaaaaaaaaaa</h1>
-            <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBBBaaaaaaaaaaaaaaa</h1>
-            <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBBBaaaaaaaaaaaaaa</h1>
-
+<div class="header" style="background-image: url('img/Info.png');!important; display: block; background-size: cover;">
+    <div class="side-nav">
+        <a href="#" class="logo">
+            <img src="img/LOGO33.png" class="logo-img" alt="">
+        </a>
+        <ul class="nav-links">
+            <li><a href="#"><i class="fa-solid fa-car-burst"></i>
+                    <p>Acidente</p></a></li>
+            <li><a href="#"><p>Acidente</p></a></li>
+            <li><a href="#"><p>Acidente</p></a></li>
+            <li><a href="#"><p>Acidente</p></a></li>
+            <li><a href="#"><p>Acidente</p></a></li>
+            <li><a href="#"><p>Acidente</p></a></li>
+            <div class="active"></div>
+        </ul>
+    </div>
+    <i class="fa-solid fa-map-location-dot" style="position: absolute"></i>
+    <div class="container">
+        <div class="cold-md-6">
+            <div class="ecra">
+                <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBB-------------------Baaaaaaaaaaaaaaaaaa</h1>
+                <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBBBaaaaaaaaaaaaaaa</h1>
+                <h1>AAAAAAAAAAAAAAAAAAABBBBBBBBBBaaaaaaaaaaaaaa</h1>
+            </div>
         </div>
     </div>
-    </body>
+</div>
+</body>
 </html>
